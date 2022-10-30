d324cea5715be21f6c4ec61ceb040aa6b13d4a8b
adityabhartib <adityabharti.ab@gmail.com>
2022-10-30 09:16:55 +0530

    Added :Refactor_all_the_3_to_One_Generic_Method

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,25 +1,38 @@
 package com.bridgelabz;
 
-public class TestMaximum {
-    public static String compareTo(String str1,String str2,String str3) {
-        if(str1.compareTo(str2) > 0 ) {
-            if(str1.compareTo(str3) > 0) {
-                return str1;
-            } else {
-                return str3;
-            }
-        } else {
-            if(str2.compareTo(str3) > 0) {
-                return str2;
-            } else {
-                return str3;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestMaximum<T extends Comparable<T>> {
+
+        ArrayList<T> list = new ArrayList<T>();
+
+        TestMaximum(T... inputs) {
+
+            for (T value : inputs) {
+                this.list.add(value);
             }
         }
-    }
 
-    public static void main(String[] args) {
-        System.out.println("----!Welcome to Generic Test Maximum!-----");
-        String maxString=compareTo("Banana","Apple","Peach");
-        System.out.println("Maximum from 3 String : "+maxString);
+        public T testMaximum() {
+            return TestMaximum.testMaximum(list);
+        }
+
+        public static <T extends Comparable<T>> T testMaximum(ArrayList<T> list) {
+            Collections.sort(list);
+            T maxValue = list.get(list.size() - 1);
+            System.out.println("Maximum value: " + maxValue);
+            return maxValue;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("----!Welcome to Generic Test Maximum!-----");
+            Integer intValue1 = 30, intValue2 = 50, intValue3 = 90;
+            Float floatValue1 = 3.3f, floatValue2 = 3.5f, floatValue3 = 8.7f;
+            String stringValue1 = "Apple", stringValue2 = "Peach", stringValue3 = "Banana";
+
+            new TestMaximum(intValue1, intValue2, intValue3).testMaximum();
+            new TestMaximum(floatValue1, floatValue2, floatValue3).testMaximum();
+            new TestMaximum(stringValue1, stringValue2, stringValue3).testMaximum();
+        }
     }
-}
