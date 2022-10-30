bfa83d9f9c8ef52f7c89bed19f12f28c971f1b01
adityabhartib <adityabharti.ab@gmail.com>
2022-10-30 08:36:56 +0530

    Added :max string at third position

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -19,7 +19,7 @@ public class TestMaximum {
 
     public static void main(String[] args) {
         System.out.println("----!Welcome to Generic Test Maximum!-----");
-        String maxString=compareTo("Banana","Peach","Apple");
+        String maxString=compareTo("Banana","Apple","Peach");
         System.out.println("Maximum from 3 String : "+maxString);
     }
 }
